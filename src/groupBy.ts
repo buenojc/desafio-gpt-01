3a7bbcb0a0bb11c054242f13a2681efcf7d52b2b
Julio Cesar Bueno <buenojc@outlook.com>
2025-06-19 23:21:33 -0300

    Fix groupBy with generic types

--- src/groupBy.ts
+++ src/groupBy.ts
@@ -1,32 +1,47 @@
-type itemArray = {
+type User = {
     name: string;
-    [attr: string]: any;
+    age: number;
 }
 
-type organizeditem = {
-    [criteria: string]: itemArray[]
-}
-
-const data: itemArray[] = [
+const data: User[] = [
     {name: 'Alice', age: 21},
     {name: 'Bob', age: 25},
     {name: 'Charlie', age: 21},
 ]
 
-
-function groupBy(array: any[], callback: Function) {
-    let organizedItems: organizeditem = {}
-    array.forEach((item: itemArray) => {
-        const criteria: string = callback(item)
-        const criteriaInUse = criteria in organizedItems
-        if (criteriaInUse) {
-            organizedItems[criteria].push(item)
-        } else {
-            organizedItems[criteria] = [item]
+function groupBy<T, K extends keyof T>(
+    array: T[],
+    callback: (item: T) => T[K]
+): Record<T[K] & (string | number | symbol), T[]>{
+    let organizedItems:Record<T[K] & (string | number | symbol), T[]> = {} as any
+    array.forEach((item: T) => {
+        const separator: T[K] = callback(item)
+        const separatorKey = separator as T[K] & (string | number | symbol)
+        if(separatorKey in organizedItems) {
+            organizedItems[separatorKey].push(item)
+        }else{
+            organizedItems[separatorKey] = [item]
         }
     })
-    return organizedItems
+    return organizedItems;
 }
 
-const fruits: organizeditem = groupBy(data, (item: itemArray) => item.age)
-console.log(fruits)
+const groupedByAge = groupBy(data, item => item.age)
+console.log(groupedByAge)
+
+// function groupBy(array: any[], callback: Function) {
+//     let organizedItems: organizeditem = {}
+//     array.forEach((item: itemArray) => {
+//         const criteria: string = callback(item)
+//         const criteriaInUse = criteria in organizedItems
+//         if (criteriaInUse) {
+//             organizedItems[criteria].push(item)
+//         } else {
+//             organizedItems[criteria] = [item]
+//         }
+//     })
+//     return organizedItems
+// }
+//
+// const fruits: organizeditem = groupBy(data, (item: itemArray) => item.age)
+// console.log(fruits)
